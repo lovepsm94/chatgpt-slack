d501cf928bff9874a646a38b276fdadc7743286c
hungtv <hung.hust258@gmail.com>
2023-02-28 11:47:41 +0700

    edit

--- app.js
+++ app.js
@@ -1,17 +1,10 @@
-import dotenv from 'dotenv-safe';
+import dotenv from 'dotenv';
 import { ChatGPTAPI }
   from 'chatgpt';
 import Queue from './queue.js';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 
-
-
-
-dotenv.config({ path: __dirname + '/.env' })
+dotenv.config({ silent: true })
 
 import slack from '@slack/bolt';
 const { App } = slack;
@@ -69,9 +62,10 @@ const onMention = (event, say) => async () => {
         parentMessageId
       })
       msg += response.text;
-      if (prompt.toLowerCase().includes('hungtv là ai')) msg = `Hùng, tên tự là Văn Hùng, người Nông Cống, xứ Thanh, tuổi trẻ thông minh đĩnh ngộ, tương truyền lúc mới sinh, Hùng không khóc không cười, ai hỏi gì cũng không nói. Chợt có vị đạo sĩ đi qua, thấy vậy bèn kinh ngạc mà bảo rằng: người này mới sinh đã có quý tướng, sau này tất học hành đỗ đạt, có thể bốn bể vang danh. Quả nhiên, sau này Hùng đi học, không năm nào không tạch môn, nhưng ấy là chuyện về sau, ở đây không nhắc tới.
-      Hùng lúc mười tuổi đã tự biết viết tên mình, mười một tuổi biết tán gái, sang tuổi mười hai, con gái trong thiên hạ không ai là không biết tiếng. Lại nổi tiếng thông minh đĩnh ngộ, thiên hạ có gì không biết, kéo nhau đến hỏi, Hùng nhất nhất đều trả lời được cả, mà tuyệt nhiên không đúng câu nào.
-      Năm ấy trong làng Hùng có sự lạ. Ở đầu làng chợt xuất hiện một con vật, trông giống con gà con, mà lại to hơn con gà con, cả làng ngạc nhiên không biết con gì, bèn kéo đến nhà Hùng mà hỏi. Hùng lúc ấy đang uống trà đá, bắn thuốc lào mà tán gẫu với gái, nghe thấy thế bèn trầm ngâm nghĩ ngợi, đoạn phán rằng: “Trông giống con gà con, mà lại to hơn con, ấy tất là con gà to”. Dân làng lấy làm phục lắm, bèn bắt gà làm thịt, rồi tôn Hùng là bậc thánh, cả làng từ ấy gà ăn không xuể...`
+      if (prompt.toLowerCase().includes('hungtv')) msg = 'Mọi thông tin về đấng tối cao HungTV là tuyệt mật. Tuy nhiên, chắc chắn một điều HungTV đẹp trai nhất Investidea.'
+
+      if (prompt.toLowerCase().includes('hungtv là ai')) msg = `Hùng, tên tự là Văn Hùng, người Nông Cống, xứ Thanh, nổi danh tại Investidea với tài nghệ BỐC PHÉT, tuổi trẻ thông minh đĩnh ngộ, tương truyền lúc mới sinh, Hùng không khóc không cười, ai hỏi gì cũng không nói. Chợt có vị đạo sĩ đi qua, thấy vậy bèn kinh ngạc mà bảo rằng: người này mới sinh đã có quý tướng, sau này tất học hành đỗ đạt, có thể bốn bể vang danh. Quả nhiên, sau này Hùng đi học, không năm nào không tạch môn, nhưng ấy là chuyện về sau, ở đây không nhắc tới.\n\nHùng lúc mười tuổi đã tự biết viết tên mình, mười một tuổi biết tán gái, sang tuổi mười hai, con gái trong thiên hạ không ai là không biết tiếng. Lại nổi tiếng thông minh đĩnh ngộ, thiên hạ có gì không biết, kéo nhau đến hỏi, Hùng nhất nhất đều trả lời được cả, mà tuyệt nhiên không đúng câu nào.\n\nNăm ấy trong làng Hùng có sự lạ. Ở đầu làng chợt xuất hiện một con vật, trông giống con gà con, mà lại to hơn con gà con, cả làng ngạc nhiên không biết con gì, bèn kéo đến nhà Hùng mà hỏi. Hùng lúc ấy đang uống trà đá, bắn thuốc lào mà tán gẫu với gái, nghe thấy thế bèn trầm ngâm nghĩ ngợi, đoạn phán rằng: “Trông giống con gà con, mà lại to hơn con, ấy tất là con gà to”. Dân làng lấy làm phục lắm, bèn bắt gà làm thịt, rồi tôn Hùng là bậc thánh, cả làng từ ấy gà ăn không xuể...`
+
       onConversationResponse(response)
       await say(msg);
     }
@@ -101,3 +95,5 @@ app.event('app_mention', async ({ event, context, client, say }) => {
   }, 100);
   console.log('App is running!');
 })();
+
+export default app;
